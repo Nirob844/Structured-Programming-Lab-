83befb6a0b24decf7b9352fb94895a24672fffcd
Nirobhasan844 <105924836+Nirobhasan844@users.noreply.github.com>
2022-05-21 00:30:10 +0600

    Update problem 8.c

--- Nirob/lab 1/problem 8.c
+++ Nirob/lab 1/problem 8.c
@@ -1,12 +1,22 @@
-#include <stdio.h>
+#include<stdio.h>
 int main()
 {
-   int i,num1;
-   char num[5];
-   gets(num);
+int a,b,c,d,e,d1,d2,d3,d4,d5,sum,number;
 
-   for(i=4;i>=0;i--){
-     printf("%d",num[i]-48);
-   }
+printf("Enter a five digit number : ");
+scanf("%d",&number);
 
-}
+a=number/10;
+d5=number%10;
+b=a/10;
+d4=a%10;
+c=b/10;
+d3=b%10;
+d=c/10;
+d2=c%10;
+e=d/10;
+d1=d%10;
+
+printf("The reverce oder is  %d \t %d \t %d \t %d \t %d \n",d5,d4,d3,d2,d1);
+	
+	return 0;
