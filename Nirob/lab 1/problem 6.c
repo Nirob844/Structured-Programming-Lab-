6758abcf5d813b77e5ba7abae1a2b9ea88a0b0c3
Nirob Hasan <105924836+Nirobhasan844@users.noreply.github.com>
2022-06-06 18:26:27 +0600

    Delete Nirob directory

--- Nirob/lab 1/problem 6.c
+++
@@ -1,9 +0,0 @@
-#include<stdio.h>
-int main()
-{
-	int c,d;
-	printf("Enter Two Number For Interchange: \n");
-	scanf("%d%d",&c,&d);
-	printf("After Interchange Output Is %d %d\n",d,c);
-	return 0;
-}
